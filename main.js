5e3255b1c76d84ba83d00304b2a35e02c46bd9ce
Naka <bacondoor1313@gmail.com>
2024-11-25 22:07:23 +0900

    therd

--- main.js
+++ main.js
@@ -50,6 +50,7 @@ function init() {
         const material = new THREE.MeshStandardMaterial({ vertexColors: true });
         mesh = new THREE.Mesh(geometry, material);
         mesh.position.set(0, 0, -2); // カメラの前方に配置
+        mesh.visible = false;
         scene.add(mesh);
     }).catch((error) => {
         console.error('Failed to load PLY file:', error);
