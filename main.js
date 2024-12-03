2fe04ab457601177d8e263290957fc329f93208d
Naka <bacondoor1313@gmail.com>
2024-12-03 15:59:07 +0900

    change Onframe

--- main.js
+++ main.js
@@ -99,7 +99,7 @@ async function init() {
 
             try {
                 await viewer.addSplatScene(modelPath, {
-                    'position': [0, 0, 0], // カメラ前方2メートルに配置
+                    'position': reticle.position, // カメラ前方2メートルに配置
                     'scale': [0.1, 0.1, 0.1],
                     'rotation': [0, 0, 0, 1]
                 }).then(() => {
