e5085fca3207bba0a6df50853a6f112450e4c9f8
Naka <bacondoor1313@gmail.com>
2024-12-03 12:05:28 +0900

    change method

--- main.js
+++ main.js
@@ -1,124 +1,165 @@
-import * as THREE from 'three';
-import { ARButton } from 'ARButton';
-import * as GaussianSplats3D from 'gaussian-splats-3d'; // 利用するライブラリを読み込む
+import * as SPLAT from "gsplat";
+import * as THREE from "three";
 
+const scale = 1
+const movement_scale = 5
+const initial_z = 14
 
-let camera, scene, renderer;
-let reticle;
-let viewer;
+let trenderer, xrRefSpace, tscene, tcamera;
+const scene = new SPLAT.Scene();
+const camera = new SPLAT.Camera(
+    new SPLAT.Vector3(0, 0, -5),
+    new SPLAT.Quaternion(),
+    2232 / 4,
+    2232 / 4,
+    0.03,
+    100
+)
+const renderer = new SPLAT.WebGLRenderer();
+// remove background color from renderer
+renderer.domElement.style.background = "unset";
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-init();
-
-async function init() {
-    // シーンの作成
-    scene = new THREE.Scene();
-
-    // カメラの作成
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
-
-    // レンダラーの作成
-    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.xr.enabled = true;
-    document.body.appendChild(renderer.domElement);
-    const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(0, 10, 10);
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // 環境光を追加
-
-    scene.add(light,ambientLight);
-    // ARボタンの追加
-    const sessionInit = { requiredFeatures: ['hit-test'], optionalFeatures: ['local-floor', 'bounded-floor'] };
-    document.body.appendChild(ARButton.createButton(renderer, sessionInit));
-
-    // Gaussian Splats 3D Viewer の初期化
-    viewer = new GaussianSplats3D.Viewer({
-        
-        'webXRMode': GaussianSplats3D.WebXRMode.AR
+async function convertPLYToSPLAT(url) {
+    // Load PLY file into scene
+    await SPLAT.PLYLoader.LoadAsync(url, scene, (progress) => {
+        console.log("Loading ply file: " + progress);
     });
-
-    // Gaussian Splats モデルのロード
-    const modelPath = './assets/fazzino3D.compressed.splat';
-
-    // レティクルの作成
-    const reticleGeometry = new THREE.RingGeometry(0.15, 0.2, 32).rotateX(-Math.PI / 2);
-    const reticleMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-    reticle = new THREE.Mesh(reticleGeometry, reticleMaterial);
-    reticle.matrixAutoUpdate = false;
-    reticle.visible = false;
-    scene.add(reticle);
-
-    // ヒットテストソースの設定
-    let hitTestSource = null;
-    let hitTestSourceRequested = false;
-
-    renderer.setAnimationLoop((timestamp, frame) => {
-        if (frame) {
-            const referenceSpace = renderer.xr.getReferenceSpace();
-            const session = renderer.xr.getSession();
-
-            if (!hitTestSourceRequested) {
-                session.requestReferenceSpace('viewer').then((space) => {
-                    session.requestHitTestSource({ space }).then((source) => {
-                        hitTestSource = source;
-                    });
-                }).catch((error) => {
-                    console.error('Failed to request hit test source:', error);
-                });
-
-                session.addEventListener('end', () => {
-                    hitTestSourceRequested = false;
-                    hitTestSource = null;
-                });
-                hitTestSourceRequested = true;
-            }
-
-            if (hitTestSource) {
-                const hitTestResults = frame.getHitTestResults(hitTestSource);
-                if (hitTestResults.length > 0) {
-                    const hit = hitTestResults[0];
-                    const pose = hit.getPose(referenceSpace);
-
-                    reticle.visible = true;
-                    reticle.matrix.fromArray(pose.transform.matrix);
-                } else {
-                    reticle.visible = false;
-                }
-            }
-        }
-
-        renderer.render(scene, camera);
-    });
-    let isSceneLoading = false;
-
-    window.addEventListener('click', async () => {
-        if (reticle.visible && !isSceneLoading) {
-            isSceneLoading = true;
-            const position = new THREE.Vector3();
-            reticle.getWorldPosition(position);
-            console.log('Reticle position:', position);
-
-            try {
-                await viewer.addSplatScene(modelPath, {
-                    'position': [0, 0, 0], // カメラ前方2メートルに配置
-                    'scale': [3, 3, 3],
-                    'rotation': [0, 0, 0, 1]
-                }).then(() => {
-                    console.log('Model successfully loaded.');
-                }).catch(error => {
-                    console.error('Error loading model:', error);
-                });
-                
-                console.log('Starting viewer...');
-                viewer.start();
-                console.log('Viewer started.');
-                console.log('Reticle position:', reticle.position);
-                console.log('Reticle world position:', reticle.getWorldPosition(new THREE.Vector3()));
-               
-            } catch (error) {
-                console.error('Failed to place Gaussian Splats model:', error);
-            } finally {
-                isSceneLoading = false; // ロード完了後にフラグをリセット
-            }
-        }
-    });
+    scene.rotate(new SPLAT.Quaternion(-1, 0, 0, 0))
+    scene.scale(new SPLAT.Vector3(12*scale, 12*scale, 12*scale))
+    // Scene.data is in SPLAT format
+    return scene.data;
 }
+
+function getXRSessionInit(mode, options) {
+    if ( options && options.referenceSpaceType ) {
+        trenderer.xr.setReferenceSpaceType( options.referenceSpaceType );
+    }
+    var space = (options || {}).referenceSpaceType || 'local-floor';
+    var sessionInit = (options && options.sessionInit) || {};
+
+    // Nothing to do for default features.
+    if ( space == 'viewer' )
+        return sessionInit;
+    if ( space == 'local' && mode.startsWith('immersive' ) )
+        return sessionInit;
+
+    // If the user already specified the space as an optional or required feature, don't do anything.
+    if ( sessionInit.optionalFeatures && sessionInit.optionalFeatures.includes(space) )
+        return sessionInit;
+    if ( sessionInit.requiredFeatures && sessionInit.requiredFeatures.includes(space) )
+        return sessionInit;
+
+    var newInit = Object.assign( {}, sessionInit );
+    newInit.requiredFeatures = [ space ];
+    if ( sessionInit.requiredFeatures ) {
+        newInit.requiredFeatures = newInit.requiredFeatures.concat( sessionInit.requiredFeatures );
+    }
+    return newInit;
+ }
+
+function init(){
+    tscene = new THREE.Scene();
+    tcamera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.01, 50 );
+    trenderer = new THREE.WebGLRenderer( { antialias: true } );
+    trenderer.setPixelRatio( window.devicePixelRatio );
+    trenderer.setSize( window.innerWidth, window.innerHeight );
+    trenderer.xr.enabled = true;
+}
+
+
+function AR(){
+  var currentSession = null;
+  function onSessionStarted( session ) {
+      session.addEventListener( 'end', onSessionEnded );
+      trenderer.xr.setSession( session );
+      button.style.display = 'none';
+      button.textContent = 'EXIT AR';
+      currentSession = session;
+      session.requestReferenceSpace('local').then((refSpace) => {
+        xrRefSpace = refSpace;
+        session.requestAnimationFrame(onXRFrame);
+      });
+  }
+  function onSessionEnded( /*event*/ ) {
+      currentSession.removeEventListener( 'end', onSessionEnded );
+      trenderer.xr.setSession( null );
+      button.textContent = 'ENTER AR' ;
+      currentSession = null;
+  }
+  if ( currentSession === null ) {
+
+      let options = {
+        requiredFeatures: ['dom-overlay'],
+        domOverlay: { root: document.body },
+      };
+      var sessionInit = getXRSessionInit( 'immersive-ar', {
+          mode: 'immersive-ar',
+          referenceSpaceType: 'local', // 'local-floor'
+          sessionInit: options
+      });
+    //   fails on http
+      (navigator.xr).requestSession( 'immersive-ar', sessionInit ).then( onSessionStarted );
+  } else {
+      currentSession.end();
+  }
+  trenderer.xr.addEventListener('sessionstart',
+      function(ev) {
+          console.log('sessionstart', ev);
+      });
+  trenderer.xr.addEventListener('sessionend',
+      function(ev) {
+          console.log('sessionend', ev);
+      });
+}
+
+function onXRFrame(t, frame) {
+  const session = frame.session;
+  session.requestAnimationFrame(onXRFrame);
+  const baseLayer = session.renderState.baseLayer;
+  const pose = frame.getViewerPose(xrRefSpace);
+
+  trenderer.render( tscene, tcamera );  
+  camera._position.x = scale*movement_scale*tcamera.position.x;
+  camera._position.y = -scale*movement_scale*tcamera.position.y-1;
+  camera._position.z = -scale*movement_scale*tcamera.position.z-initial_z;
+  camera._rotation.x = tcamera.quaternion.x;
+  camera._rotation.y = -tcamera.quaternion.y;
+  camera._rotation.z = -tcamera.quaternion.z;
+  camera._rotation.w = tcamera.quaternion.w;
+}
+
+async function main() {
+
+    // standard gaussian splat example
+    // const url = "https://huggingface.co/datasets/dylanebert/3dgs/resolve/main/bonsai/bonsai-7k.splat";
+    // await SPLAT.Loader.LoadAsync(url, scene, () => {});
+
+    // dreamgaussian example
+    const url = "./assets/fazzino3D.compressed.splat";
+    const data = await convertPLYToSPLAT(url);
+
+    const frame = () => {
+        renderer.render(scene, camera);
+        requestAnimationFrame(frame);
+    };
+
+    requestAnimationFrame(frame);
+}
+
+init()
+
+function onWindowResize() {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+window.addEventListener("resize", onWindowResize);
+
+var button = document.createElement( 'button' );
+button.id = 'ArButton'
+button.textContent = 'ENTER AR' ;
+button.style.cssText+= `position: absolute;top:80%;left:40%;width:20%;height:2rem;`;
+    
+document.body.appendChild(button)
+button.addEventListener('click',x=>AR())
+
+main();
