c6a67ab9046f0f7f34b2b3fbcb691b9b8329294b
Naka <bacondoor1313@gmail.com>
2024-12-12 13:02:51 +0900

    good

--- main.js
+++ main.js
@@ -3,17 +3,7 @@ import * as GaussianSpalt3D from 'gaussian-splats-3d';
 import {ARButton} from 'ARButton'
 
 async function init() {
-  // 1. WebGL Rendererの作成
-  const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.xr.enabled = true;
-  document.body.appendChild(renderer.domElement);
-
-
-
-  // 3. シーンとカメラの設定
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 100);
+  
 
   // 4. Gaussian-Splats Viewerの設定
   const viewer = new GaussianSpalt3D.Viewer({
